9d640f7ee9b1872ee7f3129f1a11d25c81f90821
FelipeHernandez98 <andresfelipehc@ufps.edu.co>
2021-12-26 16:26:57 -0500

    Acomodando vistas y primera consulta

--- src/routes/preguntas.js
+++ src/routes/preguntas.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
 
-router.get('/categoria1', (req, res)=>{
+router.get('/categoria1', async(req, res)=>{
+    const preguntas = await pool.query('SELECT * FROM categoria1');
+    console.log(preguntas[0].pregunta);
+    const numero = Math.random() * (4 - 0 + 1) + 0;
+    console.log(numero)
+    const pregunta = preguntas[numero];
+    console.log(pregunta)
     res.render('preguntas/categoria1');
 });
 

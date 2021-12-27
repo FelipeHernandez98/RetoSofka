b4bfe72654086b1c1c3625b80c484bf7f992cd33
FelipeHernandez98 <andresfelipehc@ufps.edu.co>
2021-12-27 16:17:59 -0500

    solucionado error de inicio

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.send('Hola');
+    res.redirect('/preguntas/categoria1')
 });
 
 module.exports = router;

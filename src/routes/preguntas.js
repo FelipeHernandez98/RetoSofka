336613b7bd1a5fd94f1decb7e264c31c6ce608ab
FelipeHernandez98 <andresfelipehc@ufps.edu.co>
2021-12-27 15:13:24 -0500

    Validaciones listas

--- src/routes/preguntas.js
+++ src/routes/preguntas.js
@@ -40,16 +40,16 @@ router.post('/categoria1/:respuestaCorrecta', async(req, res)=>{
     
     if(respuesta == respuestaCorrecta){
         req.flash('success', 'Correcto tienes +100 puntos');
-        res.render('preguntas/categoria2');
+        res.redirect('/preguntas/categoria2');
     }else{
         req.flash('message', 'Incorrecto, pierdes todos los puntos');
-        res.redirect('preguntas/categoria1');
+        res.redirect('/preguntas/categoria1');
+        
     }
 });
 
 router.get('/categoria2', async (req, res) => {
 
-    console.log('Entre');
     const preguntas = await pool.query('SELECT * FROM categoria2');
     var numero = Math.random() * (4 - 0 + 1) + 0;
     numero = parseInt(numero, 10);
@@ -58,7 +58,7 @@ router.get('/categoria2', async (req, res) => {
     const respuestas = await pool.query('SELECT * FROM respuestas');
     var respuestaCorrecta = '';
     for (var i = 0; i < respuestas.length; i++) {
-        if (respuestas[i].id_pregunta == preguntaC2.id_pregunta && respuestas[i].categoria == 1) {
+        if (respuestas[i].id_pregunta == preguntaC2.id_pregunta && respuestas[i].categoria == 2) {
             respuestaCorrecta = respuestas[i].respuesta;
         }
     }
@@ -85,16 +85,17 @@ router.post('/categoria2/:respuestaCorrecta', async(req, res)=>{
     respuestaCorrecta = respuestaCorrecta.respuestaCorrecta;
     
     if(respuesta == respuestaCorrecta){
-        res.render('preguntas/categoria3');
+        req.flash('success', 'Correcto tienes +100 puntos');
+        res.redirect('/preguntas/categoria3');
     }else{
         req.flash('message', 'Incorrecto, pierdes todos los puntos');
-        res.redirect('/');
+        res.redirect('/preguntas/categoria1');
+        
     }
 });
 
 router.get('/categoria3', async (req, res) => {
 
-    console.log('Entre');
     const preguntas = await pool.query('SELECT * FROM categoria3');
     var numero = Math.random() * (4 - 0 + 1) + 0;
     numero = parseInt(numero, 10);
@@ -103,7 +104,7 @@ router.get('/categoria3', async (req, res) => {
     const respuestas = await pool.query('SELECT * FROM respuestas');
     var respuestaCorrecta = '';
     for (var i = 0; i < respuestas.length; i++) {
-        if (respuestas[i].id_pregunta == preguntaC3.id_pregunta && respuestas[i].categoria == 1) {
+        if (respuestas[i].id_pregunta == preguntaC3.id_pregunta && respuestas[i].categoria == 3) {
             respuestaCorrecta = respuestas[i].respuesta;
         }
     }
@@ -120,21 +121,118 @@ router.get('/categoria3', async (req, res) => {
     var res3 = respuestasPregunta[2];
     
 
-    res.render('preguntas/categoria2', { preguntaC3, respuestaCorrecta, res1, res2, res3 });
+    res.render('preguntas/categoria3', { preguntaC3, respuestaCorrecta, res1, res2, res3 });
 });
 
-router.post('/categoria2/:respuestaCorrecta', async(req, res)=>{
+router.post('/categoria3/:respuestaCorrecta', async(req, res)=>{
     var respuesta = req.body;
     var respuestaCorrecta = req.params;
     respuesta = respuesta.grupo1;
     respuestaCorrecta = respuestaCorrecta.respuestaCorrecta;
     
     if(respuesta == respuestaCorrecta){
-        res.render('preguntas/categoria3');
+        req.flash('success', 'Correcto tienes +100 puntos');
+        res.redirect('/preguntas/categoria4');
     }else{
         req.flash('message', 'Incorrecto, pierdes todos los puntos');
-        res.redirect('/');
+        res.redirect('/preguntas/categoria1');
+        
     }
 });
 
+router.get('/categoria4', async (req, res) => {
+
+    const preguntas = await pool.query('SELECT * FROM categoria4');
+    var numero = Math.random() * (4 - 0 + 1) + 0;
+    numero = parseInt(numero, 10);
+    const preguntaC4 = preguntas[numero];
+
+    const respuestas = await pool.query('SELECT * FROM respuestas');
+    var respuestaCorrecta = '';
+    for (var i = 0; i < respuestas.length; i++) {
+        if (respuestas[i].id_pregunta == preguntaC4.id_pregunta && respuestas[i].categoria == 4) {
+            respuestaCorrecta = respuestas[i].respuesta;
+        }
+    }
+
+    var respuestasPregunta = {};
+    for (var i = 0; i < 3; i++) {
+        var k = Math.random() * (25 - 0 + 1) + 0;
+        k = parseInt(k, 10);
+        respuestasPregunta[i] = respuestas[k].respuesta;
+    }
+
+    var res1 = respuestasPregunta[0];
+    var res2 = respuestasPregunta[1];
+    var res3 = respuestasPregunta[2];
+    
+
+    res.render('preguntas/categoria4', { preguntaC4, respuestaCorrecta, res1, res2, res3 });
+});
+
+router.post('/categoria4/:respuestaCorrecta', async(req, res)=>{
+    var respuesta = req.body;
+    var respuestaCorrecta = req.params;
+    respuesta = respuesta.grupo1;
+    respuestaCorrecta = respuestaCorrecta.respuestaCorrecta;
+    
+    if(respuesta == respuestaCorrecta){
+        req.flash('success', 'Correcto tienes +100 puntos');
+        res.redirect('/preguntas/categoria5');
+    }else{
+        req.flash('message', 'Incorrecto, pierdes todos los puntos');
+        res.redirect('/preguntas/categoria1');
+        
+    }
+});
+
+router.get('/categoria5', async (req, res) => {
+
+    const preguntas = await pool.query('SELECT * FROM categoria5');
+    var numero = Math.random() * (4 - 0 + 1) + 0;
+    numero = parseInt(numero, 10);
+    const preguntaC5 = preguntas[numero];
+
+    const respuestas = await pool.query('SELECT * FROM respuestas');
+    var respuestaCorrecta = '';
+    for (var i = 0; i < respuestas.length; i++) {
+        if (respuestas[i].id_pregunta == preguntaC5.id_pregunta && respuestas[i].categoria == 5) {
+            respuestaCorrecta = respuestas[i].respuesta;
+        }
+    }
+
+    var respuestasPregunta = {};
+    for (var i = 0; i < 3; i++) {
+        var k = Math.random() * (25 - 0 + 1) + 0;
+        k = parseInt(k, 10);
+        respuestasPregunta[i] = respuestas[k].respuesta;
+    }
+
+    var res1 = respuestasPregunta[0];
+    var res2 = respuestasPregunta[1];
+    var res3 = respuestasPregunta[2];
+    
+
+    res.render('preguntas/categoria5', { preguntaC5, respuestaCorrecta, res1, res2, res3 });
+});
+
+router.post('/categoria4/:respuestaCorrecta', async(req, res)=>{
+    var respuesta = req.body;
+    var respuestaCorrecta = req.params;
+    respuesta = respuesta.grupo1;
+    respuestaCorrecta = respuestaCorrecta.respuestaCorrecta;
+    
+    if(respuesta == respuestaCorrecta){
+        req.flash('success', 'Correcto tienes +100 puntos');
+        res.redirect('/preguntas/ganaste');
+    }else{
+        req.flash('message', 'Incorrecto, pierdes todos los puntos');
+        res.redirect('/preguntas/categoria1');
+        
+    }
+});
+
+router.get('/ganaste', (req, res)=>{
+    res.render('preguntas/ganaste');
+});
 module.exports = router;
